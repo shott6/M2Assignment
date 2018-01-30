b9798f389800f96e0cca7f60cd597091d985f2fa
Matthew <m.mckelvey16@gmail.com>
2018-01-30 02:18:59 -0500

    Finished Person file

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,8 +30,9 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+	  String front = input.substring(0, 2);
+	  String back = input.substring(2, input.length());
+	  return back + front;
 	}
 	
 	/**
